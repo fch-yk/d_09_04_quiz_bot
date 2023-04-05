d6b892c3edda7265048593396a939eaccd428c8c
yk <yefim_korshever@mail.ru>
2023-04-05 14:51:25 +0300

    Add echo bot
    step 3

--- tg_bot.py
+++ tg_bot.py
@@ -1,8 +1,25 @@
-import os
 import json
+import os
+
+from environs import Env
+from telegram import Update
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Здравствуйте')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
 
 def main():
+    env = Env()
+    env.read_env()
     folder_name = 'questions'
     files_names = os.listdir(path=folder_name)
     questions = {}
@@ -48,6 +65,18 @@ def main():
 
     with open('tmp.txt', 'w', encoding='UTF-8') as tmp_file:
         json.dump(questions, tmp_file, indent=4, ensure_ascii=False)
+
+    updater = Updater(env('QUIZ_BOT_TOKEN'))
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
